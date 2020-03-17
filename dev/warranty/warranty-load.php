2d89453b50373bf6ae96b53548f6390200c1ce95
DST2000 <dnkst1c@rambler.ru>
2020-03-17 14:37:24 +0300

    commit 040 warranty-load style

--- dev/warranty/warranty-load.php
+++ dev/warranty/warranty-load.php
@@ -13,18 +13,18 @@ if ($user->guest) {
 
 	if (strlen($search_id_check) == 9) {
 		echo '<h3>Для розничных покупателей:</h3>';
-		echo '<p>Для получения информации о товаре, отданном на диагностику, введите Ваш номер телефона, который Вы сообщили нам в качестве контактного в сервисном центре. Заполните девятизначный номер телефона (без +375) с кодом оператора. Например 175106060.</p>';
+		echo '<p>Для получения информации о товаре, отданном на диагностику, введите Ваш номер телефона, который Вы сообщили нам в качестве контактного в сервисном центре. Заполните девятизначный номер телефона (без +375) с кодом оператора. Например для городсого номера в Минске <b>175106060</b>, МТС <b>333514747</b>, А1 <b>293514747</b>.</p>';
 		echo '<div>Бланк заявления для физических лиц в формате <a href='.'"'.'media/docs/zayavleniye_fiz_lic.pdf'.'"'.' target='.'"'.'_blank'.'"'.' rel='.'"'.'noopener noreferrer'.'"'.'> .pdf</a> , <a href='.'"'.'media/docs/zayavleniye_fiz_lic.doc'.'"'.' target='.'"'.'_blank'.'"'.' rel='.'"'.'noopener noreferrer'.'"'.'> .doc</a></div>';
 		
 		$search_id = $search_id_check;
 	} elseif (strlen($search_id_check) != 9) {
 		echo '<h3>Для розничных покупателей:</h3>';
-		echo '<p>Для получения информации о товаре, отданном на диагностику, введите Ваш номер телефона, который Вы сообщили нам в качестве контактного в сервисном центре. Заполните девятизначный номер телефона (без +375) с кодом оператора. Например 175106060.</p>';
+		echo '<p>Для получения информации о товаре, отданном на диагностику, введите Ваш номер телефона, который Вы сообщили нам в качестве контактного в сервисном центре. Заполните девятизначный номер телефона (без +375) с кодом оператора. Например для городского номера в Минске <b>175106060</b>, МТС <b>333514747</b>, А1 <b>293514747</b>.</p>';
 		echo '<div>Бланк заявления для физических лиц в формате <a href='.'"'.'media/docs/zayavleniye_fiz_lic.pdf'.'"'.' target='.'"'.'_blank'.'"'.' rel='.'"'.'noopener noreferrer'.'"'.'> .pdf</a> , <a href='.'"'.'media/docs/zayavleniye_fiz_lic.doc'.'"'.' target='.'"'.'_blank'.'"'.' rel='.'"'.'noopener noreferrer'.'"'.'> .doc</a></div>';
 	}
 	?>
 	<form action="<?php echo JRoute::_('index.php?option=com_content&view=article&id=59'); ?>" method="post">
-	<p>Введите номер телефона (175106060): <input type="number" name="phonenumber" placeholder="номер телефона" value="<?php echo $search_id_check; ?>" required="required" /></p>
+	<p><b>Введите номер телефона</b> (175106060): <input type="number" class="btn-success" name="phonenumber" placeholder="НОМЕР ТЕЛЕФОНА" value="<?php echo $search_id_check; ?>" required="required" /> <i class="fas fa-phone"></i></p>
 	<p><input type="submit" /></p>
 	</form>
 	<?
