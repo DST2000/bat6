dc181d9df516d9cca6de7b2e6c93bb4f84a16758
DST2000 <41569198+DST2000@users.noreply.github.com>
2020-08-03 13:57:41 +0300

    commit 71 intro edit

--- dev/stylenew/glavnaya/intro.php
+++ dev/stylenew/glavnaya/intro.php
@@ -133,8 +133,9 @@
 
 
 
+
 		<div class="col-xs-12">
-		<button type="submit" class="btn btn-default text-center bg-red" style="border:0; color: #FFFFFF; text-shadow:0">Предложение действительно до 31.07.2020</button>
+		<button type="submit" class="btn btn-default text-center bg-red" style="border:0; color: #FFFFFF; text-shadow:0">Предложение действительно до 31.08.2020</button>
 		</div>
 	</a>
 </div>
