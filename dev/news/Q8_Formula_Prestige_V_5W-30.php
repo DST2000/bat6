2454282ce9f258f6309bd722b6a576ad98b2d9aa
DST2000 <41569198+DST2000@users.noreply.github.com>
2020-09-08 12:35:33 +0300

    commit 76 Q8 news

---
+++ dev/news/Q8_Formula_Prestige_V_5W-30.php
@@ -0,0 +1,28 @@
+<p>Новое моторное масло Q8 Formula Prestige V 5W-30 для легковых автомобилей с высокими требования к эксплуатационным характеристикам.</p>
+<hr id="system-readmore" />
+<div class="news-style">
+<p>Производитель Q8Oils выпустил новое моторное масло Q8 Formula Prestige V 5W-30 для легковых автомобилей с высокими требования к эксплуатационным характеристикам. Данный состав способствует экономии топлива, защите бензиновых и дизельных двигателей. Совместим с системами на биотопливе. Для двигателей с нормами выброса Euro 5 и Euro 6 производства группы Volkswagen AG, Mercedes-Benz (Daimler AG), Porsche и BMW.</p>
+<div class="row">
+	<div class="col-md-6">
+<p>Ключевые особенности Q8 Formula Prestige V 5W-30:</p>
+<ul>
+	<li>Подтверждённая экономя топлива на 2,28%. Проверка проводилась для двигателя Daimler M111E совместно с маслом Q8 Formula Prestige V 5W-30;</li>
+	<li>Продукт соответствует допускам VW 504.00 и 507.00 с применением новой системы испытаний Baumuster 2020.</li>
+</ul>
+<p>Масло Q8 Formula Prestige V 5W-30 соответствует следующим спецификациям:</p>
+<ul>
+	<li>ACEA C2, C3</li>
+	<li>API SP</li>
+	<li>VAG VW 501.01, VW 502.00, VW 503.00, VW 503.01, VW 504.00 Baumuster 2020, VW 505.00, VW 505.01, VW 506.00, VW 506.01, VW 507.00 Baumuster 2020</li>
+	<li>Porsche C30</li>
+	<li>BMW Longlife-04 (B48)</li>
+	<li>MB 229.31, 229.51, 229.52</li>
+</ul>	
+	</div>
+	<div class="col-md-6">
+	<p><img src="images/news/Q8-Formula-Prestige-V-5W-30.jpg" class="img-responsive" alt="Q8 Formula Prestige V 5W-30" /></p>
+	</div>
+</div>
+
+	
+</div>
