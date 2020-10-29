3b5135e46fe5994c37c2fb7e8cd8c887c2b59e4d
DST2000 <41569198+DST2000@users.noreply.github.com>
2020-10-29 17:08:21 +0300

    commit 086 user-settings

--- dev/user/User-settings.php
+++ dev/user/User-settings.php
@@ -2,28 +2,28 @@
 	<div class="col-xs-12 col-md-4">
 		<button class="btn btn-block">
 		<a href="/user-agreement">Пользовательское
-		соглашение</br></br>
+		соглашение<br /><br />
 		<sub>прочитайте и примите пользовательское соглашение</sub>
-		</br>
+		<br />
 		</a>
 		</button>
-</br>
+<br />
 	</div>
 	<div class="col-xs-12 col-md-4">
 		<button class="btn btn-block">
-			<a href="/excel">Загрузить прайс-лист</br>
-		</br>
+			<a href="/excel">Загрузить прайс-лист<br />
+		<br />
 		<sub>заполните профиль для активации загрузки</sub></a>
-</br>
+<br />
 		</button>
-</br>
+<br />
 	</div>
 	<div class="col-xs-12 col-md-4">
 		<button class="btn btn-block">
-			<a href="/catalog/user">Изменить профиль</br>
-		<sub>заполните профиль указав актуальный e-mail,</sub> </br><sub>номер и примите пользовательское соглашение</sub></a>
-</br>
+			<a href="/catalog/user">Изменить профиль<br />
+		<sub>заполните профиль указав актуальный e-mail,</sub> <br /><sub>номер и примите пользовательское соглашение</sub></a>
+<br />
 		</button>
-</br>
+<br />
 	</div>
 </div>
