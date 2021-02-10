00537c1ad4fa313072ac548629dc32dbf347b230
DST2000 <41569198+DST2000@users.noreply.github.com>
2021-02-10 14:48:24 +0300

    commit 097 2021

--- dev/footer/footer.php
+++ dev/footer/footer.php
@@ -1,43 +1,135 @@
-<section class="footer">
-    
-    	<div class="container-fluid">
-    	<div class="spacer-20"></div>
-    	<div class="row">
-    		<div class="col-md-2">
-    			<img src="media/images/logo_1.jpg" class="" style="width: 220px"  alt="logo">
-    		</div>
-    		<div class="col-md-10">
-    			<div class="footer-line red-line">
-					<img src="media/images/element_l.jpg" class="" style="high: 100px" alt="element">
-				</div>
-    		</div>
-    	</div>
-    	<div class="spacer-20"></div>
-   
-    		<div class="row">
-    			<div class="col-md-3">
-    				
-    			</div>
-    			<div class="col-md-3">
-    				<p>СООО "БАТАВТОТРЕЙД"</p>
-    				<p>УНП: 800002576</p>
-    				<p>Адрес: 223049, Минский р-н, Щомыслицкий с/с, 19/4, комн.36</p>
-					<p>Интернет-магазин аккумуляторов bat.by зарегистрирован в Торговом реестре: 17.10.2018, номер регистрации: 429375</p>
-    			</div>
-    			<div class="col-md-3">
-    				<h4><a href="<?php echo JRoute::_('catalog/filter/akkumulators/'); ?>">Аккумуляторы</a></h4>
-    				<h4><a href="<?php echo JRoute::_('catalog/filter/q8/'); ?>">Масла</a></h4>
-    				<h4><a href="<?php echo JRoute::_('catalog/lamps/'); ?>">Лампы автомобильные</a></h4>
-    				<h4><a href="<?php echo JRoute::_('catalog/schetki-stekloochistitelya/'); ?>">Щетки стеклочистительные</a></h4>
-					<h4><a href="<?php echo JRoute::_('catalog/zaryadnye-ustrojstva-i-aksessuary/'); ?>">	Зарядные устройства, тестеры, принадлежности для аккумуляторов</a></h4>
-    			</div>
-    			<div class="col-md-3">
-    				<p>Карта сайта</p>
-    				<p><a href="<?php echo JRoute::_('index.php?option=com_content&Itemid=317'); ?>">Контактная информация</a></p>
-    				<p><a href="<?php echo JRoute::_('index.php?option=com_content&Itemid=319'); ?>">Сеть "АвтоБат" &trade;</a></p>
-    				<br>
-    				<p><a href="<?php echo JRoute::_('index.php?option=com_content&view=featured&Itemid=101'); ?>"> 2019 bat.by &reg;</a></p>
-    			</div>
-    		</div>
-    	</div>
-    </section>
+<section id="bottomInfo" class="bg-grey p-0">
+<div class="container py-2 bannerBlock block-size">
+				
+<!--------->
+<div class="row row-flex">
+		<div class="col-md-4 row-flex">
+              <div class="blockBorder eco">
+                <h3 style="color: #E9522C"><i class="fas fa-car-battery"></i> Выгодно сдать аккумулятор</h3>
+                <p><strong>Утилизация.</strong> Почему аккумуляторы нужно утилизировать? Аккумулятор относится к экологически опасным отходам, которые подлежат обязательной утилизации. Аккумулятор это не просто бытовой мусор, в батарее помимо пластикового корпуса, содержится свинец, а также раствор серной кислоты (электролит).<br><br>
+
+
+<strong>Как утилизируются аккумуляторы?</strong> Старые аккумуляторы комплексно перерабатываются и все их компоненты находят повторное применение. Например, полученный после переработки и очистки свинец используется в производстве новых аккумуляторов.
+<br>
+
+Утилизация необходима, чтобы защитить природу от воздействия отравляющих веществ, содержащихся в аккумуляторах. <br><br>
+
+В ваших силах – не выбрасывать аккумулятор, проявить гражданскую ответственность. В наших – дорого принять ваш старый аккумулятор. <i class="fa fa-refresh"></i>
+				  </p>
+              </div>
+            </div>
+            <div class="col-md-4 row-flex">
+              <div class="blockBorder carts">
+                <h3 style="color: #E9522C"><i class="fas fa-credit-card"></i> Способы оплаты</h3>
+                <p><strong>Наличными курьеру</strong> (Оплаты наличными курьеру при получении) данный способ оплаты возможен при доставке заказа. Мы гарантируем бережное обращение с товаром в процессе доставки. Также Вы можете приехать за товаром самостоятельно, и получить скидку 5 рублей. <br><br>
+<strong>Оплата банковскими картами</strong> (Возможна и в магазинах, и курьеру при доставке) 
+Вы можете купить аккумулятор в рассрочку, воспользоваться нашей партнёрской программой: <br>
+<strong>Карта "Халва" </strong>- карта рассрочки "МТБанк", <br>
+<strong>«Моцная картка» </strong>- карта рассрочки.<br>
+<strong>«Карта покупок»</strong> - карта рассрочки «Белгазпромбанка»
+</p>
+              </div>
+            </div>
+            <div class="col-md-4 row-flex">
+              <div class="blockBorder deliver">
+                <h3 style="color: #E9522C"><i class="fas fa-shipping-fast"></i> Доставка аккумуляторов</h3>
+                <p>Наша компания экономит Ваше время и осуществляет доставку аккумуляторов к Вашему автомобилю. Наши специалисты не только доставят аккумулятор в короткое время, но и профессионально произведут установку аккумулятора.<br><br>
+
+<strong>Доставка по Минску в день заказа или по согласованию в другой день.<br><br>
+Оплата наличными или банковской картой.</strong></p>
+              </div>
+            </div>
+
+</div>
+<!--------->
+		
+						
+</div>
+</section>
+
+
+
+<footer class="text-muted">
+      <div class="container py-4 block-size">
+        <div class="contact-section">
+            <div class="col-md-6 py-2 different-products">
+                <ul>
+                    <li class="first"> магазин</li>
+                    -
+                    <li><a href="/catalog/akkumulators/"> аккумуляторы</a></li>
+                    |
+                    <li><a href="/catalog/q8/"> моторные масла</a></li>
+                    |
+                    <li><a href="/catalog/zaryadnye-ustrojstva-i-aksessuary/"> аксессуары</a></li>
+                </ul>
+                <ul>
+                    <li class="first"> Помощь</li>
+                    -
+                    <li><a href="/roznichnaya-set"> Доставка </a></li>
+                    |
+                    <li><a href="/roznichnaya-set"> Оплата </a></li>
+                    |
+                    <li><a href="/roznichnaya-set"> Условия гарантии</a></li>
+                    |
+                    <li><a href="/sdat-staryj-akkumulyator"> Сдать старый аккумулятор </a></li>
+                </ul>
+
+                <ul>
+                    <li class="first"> Аккаунт оптовых покупателей</li>
+                    <li> -
+                    </li>
+                    <li><a href="/optovye-pokupateli" title="Доступен оптовым пользователям"> Вход </a></li>
+                    |
+                    <li><a href="/catalog/cart"> Ваша корзина </a></li>
+                </ul>
+                <ul>
+                    <li class="first"> О компании</li>
+                    -
+                    <li><a href="/contacts"> Контакт с нами </a></li>
+                    |
+                    <li><a href="/contacts"> О нас </a></li>
+                    |
+                    <li><a href="/contacts"> Вакансии </a></li>
+                </ul>
+            </div>
+            <div class="col-md-6 py-2 about-text text-right">
+                <h4>Информация о регистрации</h4>
+                <p>Адрес: 223049, Минский р-н, Щомыслицкий с/с, 19/4, комн.36<br>
+                    Интернет-магазин аккумуляторов bat.by зарегистрирован в Торговом реестре: 17.10.2018<br>
+                    номер регистрации: 429375</p>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        
+        
+        <div class="footer-middle">
+            
+            <div class="col-xs-12 col-md-5 py-2 follow text-left">
+            <a href="index.html" style="display: block"><img src="images/logo/logo.png" alt="БатАвтотрейд" style="width: 120px; float: left; margin-right: 10px; overflow: hidden">
+                    <p style="float: left; padding-top:5px; max-width: 220px;">Аккумуляторы, масла Q8 от официального поставщика по выгодным ценам</p>
+            </a>
+            <p style="clear: both">2003-2021 © БатАвтоТрейд. Все права защищены</p>
+            </div>
+            
+             <div class="col-xs-12 col-md-3 py-0 follow text-left hidden-xs">
+                <h3>Связаться с нами</h3>
+                <p>Наше представительство в соцсетях</p>
+                <div class="social-icons">
+                    <i class="fab fa-vk"></i>
+                    <i class="fab fa-facebook"></i>
+                </div>
+            </div>
+            
+            
+            <div class="col-xs-12 col-md-4 py-0 help text-right hidden-xs">
+                <h3>Нужна помощь?</h3>
+                <p>Наши специалисты ответят на все ваши вопросы</p>
+                <a href="/contacts">связаться</a>
+            </div>
+            
+            
+            
+            <div class="clearfix"></div>
+        </div>
+      </div>
+</footer>
