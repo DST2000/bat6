1bd320811af0cf883b7eb9e7d614783cc20f26b9
DST2000 <41569198+DST2000@users.noreply.github.com>
2021-03-04 14:15:45 +0300

    commit 099 exide eg

---
+++ dev/stylenew/news/exide.php
@@ -0,0 +1,214 @@
+<span class="intro">
+					<p><img class="rounded mx-auto d-block imgnews" title="Exide EG1903" src="/images/news/exide_EG1903_e_res.jpg" alt="Exide EG1903" ></p>
+<p>Новые грузовые аккумуляторы Exide</p>
+					</span>
+<hr id="system-readmore" />
+<div class="news-style">
+<p>В последние годы владельцы крупных транспортных компании и автопарков обращают все больше внимания на общую стоимость владения транспортом. Экономия на качестве автозапчастей может привести к большим потерям.</p>
+<p>Понимая потребности своих клиентов, серьезные производители комплектующих для грузовых автомобилей постоянно работают над улучшением  своих продуктов.</p>
+<p>Так, производитель автомобильных аккумуляторов EXIDE провел работу по улучшению конструкции грузовых аккумуляторов серии Start Prо. Модельный ряд был расширен новыми типами с увеличенными пусковыми и ёмкостными характеристиками.</p>
+<div class="row">
+	<div class="col-md-6">
+	
+	</div>
+	<div class="col-md-6">
+	
+	</div>
+</div>
+<div class="row">
+<div class="col-md-4 col-md-offset-1">
+<p><img class="img-responsive" src="/images/news/exide_EG1903_e.jpg" alt="Exide EG1903"></p>
+<p class="figsign">Exide EG1903 (190 Ач, 1100А)</p>
+</div>
+<div class="col-md-4 col-md-offset-2">
+<p><img class="img-responsive" src="/images/news/exide_EG1906_rus.jpg" alt="Exide EG1906"></p>
+<p class="figsign">Exide EG1906 (190 Ач, 1100А)</p>
+</div>
+</div>		
+<div class="row">
+<div class="col-md-4 col-md-offset-1">
+<p><img class="img-responsive" src="/images/news/exide_EG2253_.jpg" alt="Exide EG2253"></p>
+<p class="figsign">Exide EG2253 (225 Ач, 1200 А)</p>
+</div>
+<div class="col-md-4 col-md-offset-2">
+<p><img class="img-responsive" src="images/news/exide_EG2254_.jpg" alt="Exide EG2254"></p>
+<p class="figsign">Exide EG2254 (225 Ач, 1200 А)</p>
+</div>
+</div>	
+<h2>Новые типы линейки StartPRO с характеристиками: </h2>	
+	
+<table width="696" class="table">
+<thead>
+<tr class="info">
+<th scope="col" width="76">
+<p><strong>Exide</strong></p>
+</th>
+<th colspan="2" width="155">
+<p><strong>Параметры</strong></p>
+</th>
+<th colspan="4" width="313">
+<p><strong>Размеры</strong></p>
+</th>
+<th colspan="2" width="152">
+<p><strong>Характеристики</strong></p>
+</th>
+</tr>
+<tr class="info">
+<th width="76">
+<p><strong>Код</strong></p>
+</th>
+<th width="77">
+<p><strong>Ёмкость</strong><strong> Ah</strong></p>
+</th>
+<th width="77">
+<p><strong>Ток</strong> <strong>CCA A (EN)</strong></p>
+</th>
+<th width="82">
+<p><strong>Блок</strong></p>
+</th>
+<td width="77">
+<p><strong>Код</strong></p>
+</td>
+<th width="77">
+<p><strong>Ёмкость</strong><strong> Ah</strong></p>
+</th>
+<th width="77">
+<p><strong>Ток</strong> <strong>CCA A (EN)</strong></p>
+</th>
+<th width="77">
+<p><strong>Блок</strong></p>
+</th>
+<th width="75">
+<p><strong>Код</strong></p>
+</th>
+</tr>	
+</thead>
+<tbody>
+<tr>
+<td width="76">
+<p>EG1903</p>
+</td>
+<td width="77">
+<p><strong>190</strong></p>
+</td>
+<td width="77">
+<p><strong>1100</strong></p>
+</td>
+<td width="82">
+<p>D05</p>
+</td>
+<td width="77">
+<p>513</p>
+</td>
+<td width="77">
+<p>223</p>
+</td>
+<td width="77">
+<p>223</p>
+</td>
+<td width="77">
+<p>ETN3</p>
+</td>
+<td width="75">
+<p>B0</p>
+</td>
+</tr>
+<tr>
+<td width="76">
+<p>EG1906</p>
+</td>
+<td width="77">
+<p><strong>190</strong></p>
+</td>
+<td width="77">
+<p><strong>1100</strong></p>
+</td>
+<td width="82">
+<p>D05</p>
+</td>
+<td width="77">
+<p>510</p>
+</td>
+<td width="77">
+<p>218</p>
+</td>
+<td width="77">
+<p>225</p>
+</td>
+<td width="77">
+<p>ETN4</p>
+</td>
+<td width="75">
+<p>B3</p>
+</td>
+</tr>
+<tr>
+<td width="76">
+<p>EG2253</p>
+</td>
+<td width="77">
+<p><strong>225</strong></p>
+</td>
+<td width="77">
+<p><strong>1200</strong></p>
+</td>
+<td width="82">
+<p>D06</p>
+</td>
+<td width="77">
+<p>518</p>
+</td>
+<td width="77">
+<p>240</p>
+</td>
+<td width="77">
+<p>279</p>
+</td>
+<td width="77">
+<p>ETN3</p>
+</td>
+<td width="75">
+<p>B0</p>
+</td>
+</tr>
+<tr>
+<td width="76">
+<p>EG2254</p>
+</td>
+<td width="77">
+<p><strong>225</strong></p>
+</td>
+<td width="77">
+<p><strong>1200</strong></p>
+</td>
+<td width="82">
+<p>D06</p>
+</td>
+<td width="77">
+<p>513</p>
+</td>
+<td width="77">
+<p>240</p>
+</td>
+<td width="77">
+<p>279</p>
+</td>
+<td width="77">
+<p>ETN4</p>
+</td>
+<td width="75">
+<p>B0</p>
+</td>
+</tr>
+</tbody>
+</table>
+<p>Аккумуляторы Exide StartPRO изготавливаются по специальной технологии HD (Heavy Duty), которая включает:</p>	
+<ul>
+	<li>использование пластин  Ca\Ca</li>
+	<li>изготовление положительных пластин по технологии штамповки Punch</li>
+	<li>изготовление отрицательные пластины по технологии тянутого металла Exmet</li>
+	<li>технологию Heavy Duty, в частности, использование стеклофлиса и усиленного сепаратора для улучшения параметра циклирования,  применение термокомпаунда для лучшей фиксации блоков пластин</li>
+	<li>доступ к электролиту и возможность контроля его уровня традиционным способом</li>
+</ul>
+<p>Благодаря технологии HD technology аккумуляторы Exide также имеют более жесткую и прочную структуру пластин; они меньше подвергаются коррозии;  и как следствие имеют увеличенный срок службы и превосходные электрические характеристики и производительность.</p>
+</div>
