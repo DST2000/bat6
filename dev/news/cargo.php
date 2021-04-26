df9e3f0cd8425f750fd6cbcf0c11f5e2216c2459
DST2000 <dnkst1c@rambler.ru>
2021-04-26 11:26:40 +0300

    commit 100 cargo

---
+++ dev/news/cargo.php
@@ -0,0 +1,15 @@
+<p><img class="rounded mx-auto d-block imgnews" title="Транспорт БАТАВТОТРЕЙД" src="images/news/bat_cargo_.jpg" alt="batavtotrade cargo" /></p>
+<p>БатАвтоТред обновляет автопарк</p>
+<hr id="system-readmore" />
+<div class="news-style">
+<p>
+Компания СООО «БатАвтоТрейд» заботится о своевременности и регулярности поставок наших аккумуляторов и масел по всей территории Республики Беларусь. Постоянно обновляемый парк коммерческого грузового транспорта является гарантией того, что наши клиенты получат товар вовремя и в полном объеме. Ознакомиться с графиком поставок можно на странице <a href="/roznichnym-pokupatelyam/46-dostavka">доставка</a>.
+</p>
+<p>На данный момент автопарк состоит из восьми грузовых автомобилей, используются следующие модели:</p>
+<ul>
+	<li>Mercedes-Benz Sprinter</li>
+	<li>Volkswagen Crafter</li>
+	<li>Renault Master</li>
+</ul>
+<p><img title="Транспорт БАТАВТОТРЕЙД" src="images/news/bat_cargo_.jpg" alt="batavtotrade cargo" /></p>	
+</div>
